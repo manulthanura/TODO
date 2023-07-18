7278510c77f6070dea6ca3ccca086f90233ca1fc
Manul Thanura <manulkarunarathna@gmail.com>
2023-07-18 09:13:25 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,346 @@
+
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
+<!-- Head -->
+<?php include "./header.php";?>
+<!-- End Head -->
+<body class="presentation-page">
+  <!-- Navbar -->
+  <?php include "./navbar.php";?>
+  <!-- Hero section -->
+  <header class="header-2">
+    <div class="page-header min-vh-75 relative" style="background-image: url('./assets/img/curved-images/curved.jpg')">
+    <span class="mask bg-gradient-primary opacity-5"></span>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7 text-center mx-auto">
+            <h1 class="text-white pt-3 mt-n5">FORGEAR</h1>
+            <p class="lead text-white mt-3">All in one solution to grow online</p>
+          </div>
+        </div>
+      </div>
+      <div class="position-absolute w-100 z-index-1 bottom-0">
+        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 40" preserveAspectRatio="none" shape-rendering="auto">
+          <defs>
+            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+          </defs>
+          <g class="moving-waves">
+            <use xlink:href="#gentle-wave" x="48" y="-1" fill="rgba(255,255,255,0.40" />
+            <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.35)" />
+            <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.25)" />
+            <use xlink:href="#gentle-wave" x="48" y="8" fill="rgba(255,255,255,0.20)" />
+            <use xlink:href="#gentle-wave" x="48" y="13" fill="rgba(255,255,255,0.15)" />
+            <use xlink:href="#gentle-wave" x="48" y="16" fill="rgba(255,255,255,0.95" />
+          </g>
+        </svg>
+      </div>
+    </div>
+  </header>
+  <!-- -------- START Features -------- -->
+  <section class="py-6">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 mx-auto text-center pb-4">
+          <h4 class="text-gradient text-primary">Welcome</h4>
+          <h2>Turn your idea into a startup</h2>
+          <p class="lead">Break free of your day-to-day and follow your passion.<br>Launch now with Forgear</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-6">
+          <div class="p-3 text-center">
+            <div class="icon icon-shape bg-gradient-info shadow mx-auto">
+              <i class="ni ni-single-02"></i>
+            </div>
+            <h4 class="mt-4">Talented team</h4>
+            <p>We try our best, and we make sure we deliver the best</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mx-md-auto">
+          <div class="p-3 text-center">
+            <div class="icon icon-shape bg-gradient-warning shadow mx-auto">
+              <i class="ni ni-atom ni-lg"></i>
+            </div>
+            <h4 class="mt-4">Amazing service</h4>
+            <p>Providing the simplest solutions for the complex problems</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="p-3 text-center">
+            <div class="icon icon-shape bg-gradient-success shadow mx-auto">
+              <i class="ni ni-email-83"></i>
+            </div>
+            <h4 class="mt-4">Support 24/7</h4>
+            <p>Building a network for the smartest people</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- -------- END Features -------- -->
+  <!-- START About -->
+  <section class="py-6">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-8 order-2 order-md-2 order-lg-1">
+          <div class="position-relative ms-md-5 mb-0 mb-md-7 mb-lg-0 d-none d-md-block d-lg-block d-xl-block h-75">
+            <div class="bg-gradient-info w-100 h-100 border-radius-xl position-absolute" alt=""></div>
+            <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/color-city.jpg" class="w-100 border-radius-xl mt-6 ms-5 position-absolute" alt="">
+          </div>
+        </div>
+        <div class="col-lg-5 col-md-12 ms-auto order-1 order-md-1 order-lg-1">
+          <div class="p-3 pt-0">
+              <div class="icon icon-shape bg-gradient-info rounded-circle shadow text-center mb-4">
+                <i class="ni ni-building"></i>
+              </div>
+              <h3 class="text-gradient text-info mb-0">We are</h3>
+              <h4 class="mb-4">A new dimension in the digital world</h4>
+              <p class="text-justify">FORGEAR is a Professional Digital Platform. Here we will provide you with only interesting solutions, which you will like very much. We're dedicated to providing you with the best of digital solutions, with a focus on dependability and software development, and IT solutions. We hope you enjoy our digital services as much as we enjoy offering them to you.<br><br>We will keep posting more important posts on our website for all of you. Please give your support and love.</p>
+              <a href="./about" class="text-dark icon-move-right">More about us
+                <i class="fas fa-arrow-right text-sm ms-1"></i>
+              </a>
+            </div>
+        </div>
+      </div>
+      <div class="row mt-6 mt-md-5 mt-lg-8">
+        <div class="col-lg-5 col-md-12 me-auto">
+          <div class="p-3 pt-0">
+              <div class="icon icon-shape bg-gradient-warning rounded-circle shadow text-center mb-4">
+                <i class="fas fa-trophy"></i>
+              </div>
+              <h3 class="text-gradient text-warning mb-0">Engineered excellence</h3>
+              <h4 class="mb-4">Using technology to empower people</h4>
+              <p class="text-justify">We are committed to client success. Our clients own the work generated by our world-class team. This allows for the long-term stability of their technology investment. <br> <br> Our goal is simple; to help clients realize their business objectives by delivering professional technology services, including strategic advice, consulting services, data analytics, or full-stack software development.</p>
+              <a href="javascript:;" class="text-dark icon-move-right">Browse more
+                <i class="fas fa-arrow-right text-sm ms-1"></i>
+              </a>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-8">
+          <div class="position-relative ms-md-5 d-none d-md-block d-lg-block d-xl-block h-75">
+            <div class="w-100 h-100 bg-gradient-warning border-radius-xl position-absolute" alt=""></div>
+            <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/arena.jpg" class="w-100 border-radius-xl mt-6 ms-n5 position-absolute" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- END About -->
+  <!-- -------- START Projects ------- -->
+  <section class="py-5">
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="row text-center my-sm-5 mt-4">
+          <div class="col-lg-6 mx-auto">
+            <h2 class="text-warning text-gradient mb-0">Boost Creativity</h2>
+            <h2 class="">With our amazing service</h2>
+            <p class="lead">The easiest way to get started is to get an idea from one of our<br> pre-built projects. </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="row mt-4">
+            <div class="col-md-6">
+              <a href="./projects">
+                <div class="card move-on-hover">
+                  <img class="w-100" src="./uploads/6252-w03.png" alt="">
+                </div>
+                <div class="mt-2 ms-2">
+                  <h6 class="mb-0">VegiMart</h6>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-6 mt-md-0 mt-5">
+              <a href="./projects">
+                <div class="card move-on-hover">
+                  <img class="w-100" src="./uploads/7988-w02.png" alt="">
+                </div>
+                <div class="mt-2 ms-2">
+                  <h6 class="mb-0">Vaccine Management System</h6>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-6 mt-md-0 mt-6">
+              <a href="./projects">
+                <div class="card move-on-hover">
+                  <img class="w-100" src="./uploads/7579-w05.png" alt="">
+                </div>
+                <div class="mt-2 ms-2">
+                  <h6 class="mb-0">Dreamweaver Flowers</h6>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-6 mt-md-0 mt-6">
+              <a href="./projects">
+                <div class="card move-on-hover">
+                  <img class="w-100" src="./uploads/4667-w01.png" alt="">
+                </div>
+                <div class="mt-2 ms-2">
+                  <h6 class="mb-0">Learning Management Systems</h6>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mx-auto mt-md-0 mt-5">
+          <div class="position-sticky" style="top:100px !important">
+            <h4 class="">What we have done so far</h4>
+            <h6 class="text-secondary">This is just a small selection of the multiple possibilities you have. Focus on the business, where innovation meets excellence.</h6><a href="./projects" class="text-primary icon-move-right">Discover more
+              <i class="fas fa-arrow-right text-sm ms-1"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- -------- END Projects ------- -->
+  <div class="container mt-sm-5">
+    <div class="page-header min-vh-50 my-sm-3 mb-3 border-radius-xl" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/desktop.jpg');">
+      <span class="mask bg-gradient-dark"></span>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 ms-lg-5">
+            <h1 class="text-white">Getting Started</h1>
+            <p class="lead text-white opacity-8">Check the possible ways of working with our team and an affordable price for building your own project.</p>
+            <a href="./service" class="text-white icon-move-right">Let's start <i class="fas fa-arrow-right text-sm ms-1"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- -------- END Projects ------- -->
+  <!-- clients -->
+  <section class="py-7">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 mx-auto text-center">
+          <h2 class="mb-0">Trusted by over</h2>
+          <h2 class="text-gradient text-primary mb-3">100+ clients</h2>
+          <p class="lead">Many Fortune companies, startups, universities, and governmental institutions love our products.</p>
+        </div>
+      </div>
+      <div class="row mt-6">
+        <div class="col-lg-4 col-md-8">
+          <div class="card card-plain">
+            <div class="card-body">
+              <div class="author">
+                <div class="name">
+                  <h6 class="mb-0 font-weight-bolder">Nick Willever</h6>
+                  <div class="stats">
+                    <i class="far fa-clock"></i> 1 week ago
+                  </div>
+                </div>
+              </div>
+              <p class="text-justify mt-4">"They have an excellent service, the outcome is excellent and helped me get things done more efficiently."</p>
+              <div class="rating mt-3">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-8 ms-md-auto">
+          <div class="card bg-gradient-primary">
+            <div class="card-body">
+              <div class="author align-items-center">
+                <div class="name">
+                  <h6 class="text-white mb-0 font-weight-bolder">Shailesh Kushwaha</h6>
+                  <div class="stats text-white">
+                    <i class="far fa-clock"></i> 2 days ago
+                  </div>
+                </div>
+              </div>
+              <p class="text-justify mt-4 text-white">"I found a solution to all my design needs from FORGEAR. Fantastic, friendly service, and it's really affordable, very humble guys !!!"</p>
+              <div class="rating mt-3">
+                <i class="fas fa-star text-white"></i>
+                <i class="fas fa-star text-white"></i>
+                <i class="fas fa-star text-white"></i>
+                <i class="fas fa-star text-white"></i>
+                <i class="fas fa-star text-white"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-8">
+          <div class="card card-plain">
+            <div class="card-body d-flex align-content-center flex-wrap">
+              <a href="./about"><h1 class="text-secondary my-6">See More<br></h1></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr class="horizontal dark mt-5 mb-auto">
+      <div class="row">
+        <div class="col-lg-2 col-md-4 col-6 ms-auto">
+          <img class="w-100 opacity-6" src="./assets/img/logos/logo-adobestock.svg" alt="Logo">
+        </div>
+        <div class="col-lg-2 col-md-4 col-6">
+          <img class="w-100 opacity-6" src="./assets/img/logos/logo-freepik.svg" alt="Logo">
+        </div>
+        <div class="col-lg-2 col-md-4 col-6">
+          <img class="w-100 opacity-6" src="./assets/img/logos/logo-fiverr.svg" alt="Logo">
+        </div>
+        <div class="col-lg-2 col-md-4 col-6 ms-lg-0 ms-md-auto">
+          <img class="w-100 opacity-6" src="./assets/img/logos/logo-freelancer.svg" alt="Logo">
+        </div>
+        <div class="col-lg-2 col-md-4 col-6 me-md-auto mx-md-0 mx-auto">
+          <img class="w-100 opacity-6" src="./assets/img/logos/logo-opensea.svg" alt="Logo">
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /clients -->
+  <!-- blog -->
+  <section class="my-auto">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7 col-12 my-auto">
+          <h3 class="text-gradient text-info mb-0">Funny experiences brought to you</h3>
+          <h3>Want more?</h3>
+          <p class="pe-md-5 mb-4">
+            The most complex and innovative Design System Made by FORGEAR. Check our latest blog. Designed for those who like bold elements and beautiful websites. Made of hundred of elements, designed blocks, and fully coded pages, FORGEAR is ready to help you create stunning websites and web apps.
+          </p>
+          <div class="github-buttons">
+            <a href="https://forgear.edu.lk/" target="_blank" rel="nofollow" class="btn bg-gradient-info mb-5 mb-sm-0">Visit Blog</a>
+            <div class="github-button">
+              <span></span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-5 col-12 my-auto">
+          <a href="https://forgear.edu.lk/">
+            <img class="w-100 border-radius-lg shadow-lg" src="./assets/img/blog.png" alt="Blog Image">
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Contact -->
+  <section class="pt-sm-7 pb-auto" id="download-soft-ui">
+    <div class="bg-gradient-dark position-relative m-3 border-radius-xl overflow-hidden">
+      <img src="./assets/img/shapes/waves-white.svg" alt="pattern-lines" class="position-absolute start-0 top-md-0 w-100 opacity-6">
+      <div class="container py-7 postion-relative z-index-2 position-relative">
+        <div class="row">
+          <div class="col-md-7 mx-auto text-center">
+            <h3 class="text-white mb-0">Do you love this awesome ?</h3>
+            <h3 class="text-info text-gradient mb-4">We would love to hear about your business and technology goals</h3>
+            <p class="text-white mb-5">People are at our core and we strive to build strong connections with everyone we encounter. These connections drive our work, as well as our involvement in the community.</p>
+            <a href="./contact" class="btn btn-info btn-lg mb-3 mb-sm-0">Let's Chat</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- -------   Footer    -------- -->
+  <?php include "./footer.php";?>
+  <!-- -------  End Footer    -------- -->
+  <?php include "./script.php";?>
+</body>
+
+</html>
