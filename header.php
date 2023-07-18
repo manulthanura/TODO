86103c3bb4164f52d54d0673fb81ef4c9c1f2f3c
Manul Thanura <manulkarunarathna@gmail.com>
2023-07-18 09:18:36 +0530

    Update

---
+++ header.php
@@ -0,0 +1,21 @@
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="We're dedicated to providing you with the best of digital solutions, with a focus on dependability and software development, and IT solutions. We hope you enjoy our digital services as much as we enjoy offering them to you.">
+  <meta name="keywords" content="forgear,forgear education,webdesign,web,app,software,documentation,graphic design">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+
+  <title>FORGEAR</title>
+
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="./assets/css/main.css?v=1.0.9" rel="stylesheet" />
+</head>
