318cae82661811e096184f2b22a88f5f8f2e0d70
Manul Thanura <manulkarunarathna@gmail.com>
2023-07-18 09:23:16 +0530

    Update footer.php

---
+++ footer.php
@@ -0,0 +1,103 @@
+
+<footer class="footer py-5 pt-5 mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 mb-4 mx-auto text-center">
+                <a href="https://forgear.lk/privacy" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    Privacy & Policy
+                </a>
+                <a href="https://forgear.lk/terms" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    Terms & Conditions
+                </a>
+                <a href="https://forgear.lk/service" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                Products
+                </a>
+                <a href="https://forgear.edu.lk/" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    Blog
+                </a>
+                <a href="https://forgear.lk/service#web-development" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    Pricing
+                </a>
+                <a href="https://forgear.lk/signin" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    Sign In
+                </a>
+            </div>
+            <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
+                <a href="https://www.facebook.com/forgearco" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-facebook" aria-hidden="true"></span>
+                </a>
+                <a href="https://www.instagram.com/forgearco/" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-instagram" aria-hidden="true"></span>
+                </a>
+                <a href="https://wa.me/94769139594" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-whatsapp" aria-hidden="true"></span>
+                </a>
+                <a href="https://www.linkedin.com/company/forgear/" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-linkedin" aria-hidden="true"></span>
+                </a>
+                <a href="https://twitter.com/ForgearC" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-twitter" aria-hidden="true"></span>
+                </a>
+                <a href="https://github.com/FORGEAR" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-github" aria-hidden="true"></span>
+                </a>
+            </div>
+        </div>
+            <div class="col-12">
+                <div class="text-center">
+                    <p class="my-4 text-sm">
+                    All rights reserved. Copyright © <script>
+                    document.write(new Date().getFullYear())
+                    </script> <br>FORGEAR
+                    </p>
+                </div>
+            </div>
+    </div>
+</footer>
+
+
+<!--   Core JS Files   -->
+<script src="https://forgear.lk/assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="https://forgear.lk/assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+<script src="https://forgear.lk/assets/js/plugins/perfect-scrollbar.min.js"></script>
+<!--  Plugin for TypedJS, full documentation here: https://github.com/inorganik/CountUp.js -->
+<script src="https://forgear.lk/assets/js/plugins/countup.min.js"></script>
+<!--  Plugin for Parallax, full documentation here: https://github.com/dixonandmoe/rellax -->
+<script src="https://forgear.lk/assets/js/plugins/rellax.min.js"></script>
+<!--  Plugin for TiltJS, full documentation here: https://gijsroge.github.io/tilt.js/ -->
+<script src="https://forgear.lk/assets/js/plugins/tilt.min.js"></script>
+<!--  Plugin for Selectpicker - ChoicesJS, full documentation here: https://github.com/jshjohnson/Choices -->
+<script src="https://forgear.lk/assets/js/plugins/choices.min.js"></script>
+<!--  Plugin for Parallax, full documentation here: https://github.com/wagerfield/parallax  -->
+<script src="https://forgear.lk/assets/js/plugins/parallax.min.js"></script>
+<!-- Control Center for Soft UI Kit: parallax effects, scripts for the example pages etc -->
+
+<!--  Google Maps Plugin    -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
+<script src="https://forgear.lk/assets/js/main.min.js?v=1.0.9" type="text/javascript"></script>
+<script type="text/javascript">
+    if (document.getElementById('state1')) {
+      const countUp = new CountUp('state1', document.getElementById("state1").getAttribute("countTo"));
+      if (!countUp.error) {
+        countUp.start();
+      } else {
+        console.error(countUp.error);
+      }
+    }
+    if (document.getElementById('state2')) {
+      const countUp1 = new CountUp('state2', document.getElementById("state2").getAttribute("countTo"));
+      if (!countUp1.error) {
+        countUp1.start();
+      } else {
+        console.error(countUp1.error);
+      }
+    }
+    if (document.getElementById('state3')) {
+      const countUp2 = new CountUp('state3', document.getElementById("state3").getAttribute("countTo"));
+      if (!countUp2.error) {
+        countUp2.start();
+      } else {
+        console.error(countUp2.error);
+      };
+    }
+  </script>
